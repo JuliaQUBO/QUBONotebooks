68237a1bdc25744bd522f33ed89b64764c2fd411
pedromxavier <pedromxavier@poli.ufrj.br>
2023-09-26 01:21:02 -0400

    Try with other packages [sysimage]

--- scripts/create_sysimage.jl
+++ scripts/create_sysimage.jl
@@ -5,28 +5,33 @@ const NOTEBOOKS_DIR = joinpath(@__DIR__, "..", "notebooks")
 const SYSIMAGE_PATH = joinpath(@__DIR__, "..", "sysimage", "sysimage.$(Libdl.dlext)")
 
 const PACKAGES = [
-    # "AmplNLWriter",
-    # "BinaryWrappers",
-    # "Bonmin_jll",
-    # "Cbc",
-    # "Couenne_jll",
-    # "DWave",
-    # "DWaveNeal",
-    # "GLPK",
-    # "Graphs",
-    # "HiGHS",
-    # "Ipopt",
-    # "JuMP",
-    # "Karnak",
+    # Misc
+    "Graphs",
+    "Karnak",
+    "SpecialFunctions",
+    # GAMA
+    "BinaryWrappers",
+    "lib4ti2_jll",
+    # JuMP
+    "AmplNLWriter",
+    "Bonmin_jll",
+    "Cbc",
+    "Couenne_jll",
+    "GLPK",
+    "HiGHS",
+    "Ipopt",
+    "JuMP",
+    # QUBO
+    "QUBO",
+    "DWave",
+    "DWaveNeal",
+    # Visualization
+    # "Plots",
     # "Measures",
-    "Plots",
-    "PythonCall",
-    "PythonPlot",
-    # "QUBO",
-    # "SpecialFunctions",
+    # "PythonCall",
+    # "PythonPlot",
     # "StatsBase",
     # "StatsPlots",
-    # "lib4ti2_jll",
 ]
 
 PackageCompiler.create_sysimage(PACKAGES; project=NOTEBOOKS_DIR, sysimage_path=SYSIMAGE_PATH)
